40966f2bf4fdd53c92754f163824209014da57d8
nicola <nicola.palmigiano@gmail.com>
2017-06-19 00:51:42 +0200

    integrazione vue,vue-loader con webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import _ from 'lodash';
-
+//import _ from 'lodash';
+import Vue from 'vue';
+import App from './app.vue'
+//import VueRouter from 'vue-router';
+/*
 function component () {
   var element = document.createElement('div');
 
@@ -10,3 +13,8 @@ function component () {
 }
 
 document.body.appendChild(component());
+*/
+new Vue({
+  el: '#app',
+  components: { App }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,27 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    // Special compilation rules
+    loaders: [
+      {
+        // Ask webpack to check: If this file ends with .js, then apply some transforms
+        test: /\.js$/,
+        // Transform it with babel
+        loader: 'babel-loader',
+        // don't transform node_modules folder (which don't need to be compiled)
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
   }
 };
